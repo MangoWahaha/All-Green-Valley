08001103797b60118f15b5d72d4c641f24da0a39
MangoWahaha <gifelsanjaya@gmail.com>
2025-06-14 14:22:26 +0700

    Menambahkan Sorting

--- JS/shop.js
+++ JS/shop.js
@@ -1,1 +1,12 @@
+const sortMenu = document.getElementById("sortMenu");
+const sortOptions = sortMenu.querySelectorAll("label");
 
+sortOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    sortOptions.forEach((o) => o.classList.remove("active"));
+
+    option.classList.add("active");
+
+    sortToggle.querySelector("span").textContent = option.textContent;
+  });
+});
